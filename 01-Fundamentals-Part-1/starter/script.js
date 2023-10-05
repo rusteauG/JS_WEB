05f468e04112e2d81ad83ebc88c8f26bd27876dd
rusteauDesktop <rrusteau@gmail.com>
2023-10-05 16:58:09 +0400

    Coding Challenge #3

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -26,27 +26,62 @@
 // lines
 // `);
 //
-const ageSarah = 15;
-
-if (ageSarah >= 18) {
-	console.log("Sarah can start driving license 😜");
-} else {
-	const ageLeft = 18 - ageSarah;
-	console.log(
-		`Sarah is too young to start driving license 😭 ${ageLeft} years left to Drive!`
-	);
-}
-
+// const ageSarah = 15;
+//
+// if (ageSarah >= 18) {
+// // console.log("Sarah can start driving license 😜");
+// } else {
+// const ageLeft = 18 - ageSarah;
+// console.log(
+// // // `Sarah is too young to start driving license 😭 ${ageLeft} years left to Drive!`
+// );
+// }
+//
 //type conversion
-const inputYear = "1991";
-console.log(Number(inputYear), inputYear);
-console.log(Number(inputYear) + 18);
-
-console.log(Number("Jonas"));
-console.log(typeof NaN);
-console.log(String(23), 23);
-
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+//
+// console.log(Number("Jonas"));
+// console.log(typeof NaN);
+// console.log(String(23), 23);
+//
 //type coercion
-console.log("I am " + 23 + " years old");
-console.log("23" - "10" - 3);
-console.log("23" + "10" + 3);
+// console.log("I am " + 23 + " years old");
+// console.log("23" - "10" - 3);
+// console.log("23" + "10" + 3);
+//
+// let fav = prompt("What's your favorite number?");
+// console.log(typeof fav);
+// if (fav != 23) {
+// console.log("23 is not THE number");
+// } else {
+// console.log("23 is  THE number");
+// }
+//
+
+//Coding Challenge #3
+let Dolphins;
+let Koalas;
+//Data#1
+//const Dolphins_Average = (96 + 108 + 89) / 3;
+//const Koalas_Average = (88 + 91 + 110) / 3;
+//Data#2
+const Dolphins_Average = (97 + 112 + 101) / 3;
+const Koalas_Average = (109 + 95 + 123) / 3;
+const minScore = 100;
+if (
+	Dolphins_Average === Koalas_Average &&
+	Dolphins_Average >= minScore &&
+	Koalas_Average >= minScore
+) {
+	console.log("Both Teams Wins 🏆");
+} else if (Dolphins_Average > Koalas_Average && Dolphins_Average >= minScore) {
+	console.log("Dolphins Win 🏆");
+} else if (Koalas_Average > Dolphins_Average && Koalas_Average >= minScore) {
+	console.log("Koalas Win 🏆");
+} else {
+	console.log("No Team Wins 🏈");
+}
+const minimum = Math.min(Dolphins_Average, Koalas_Average);
+console.log(minimum);
