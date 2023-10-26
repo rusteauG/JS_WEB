e0893e61d1f8376dc19a7768d481a08d9678cd44
rusteauDesktop <rrusteau@gmail.com>
2023-10-26 19:05:47 +0400

    Code Refactor on [     //when guess is Not equal to secret number ]

--- 04-GuessMyNumber/script.js
+++ 04-GuessMyNumber/script.js
@@ -62,25 +62,37 @@ document.querySelector('.check').addEventListener('click', function () {
       gameOver = true;
     }
 
-    //when guess is too high
-  } else if (guess > secretNumber) {
+    //when guess is Not equal to secret number
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too High!';
-      score = score - 1;
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? '📈 Too High!' : '📉 Too Low!';
+      score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '📉 You lost the game!';
-      document.querySelector('.score').textContent = 0;
-    }
-    //when guess is too low
-  } else {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too Low!';
-      score = score - 1;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '📉 You lost the game!';
+      document.querySelector('.message').textContent = '💥 You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
   }
+  //   //when guess is too high
+  // } else if (guess > secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📈 Too High!';
+  //     score = score - 1;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '📉 You lost the game!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  //   //when guess is too low
+  // } else {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📈 Too Low!';
+  //     score = score - 1;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '📉 You lost the game!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // }
 });
