c94a2e933498e0677545d0957626a02df3d42544
omnimaxn <omnimaxn@gmail.com>
2024-08-05 17:51:53 +0400

    Refactor (DRY ) DOne

--- 05-Guess-My-Number/AnotherGo/script.js
+++ 05-Guess-My-Number/AnotherGo/script.js
@@ -2,7 +2,7 @@
 const btnCheck = document.querySelector(".check");
 
 const number = document.querySelector(".number");
-const message = document.querySelector(".message");
+
 const btnAgain = document.querySelector(".again");
 const guessInput = document.querySelector(".guess"); // Store the input element
 
@@ -15,51 +15,55 @@ number.textContent = "?";
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
 btnCheck.addEventListener("click", function () {
   const guessValue = guessInput.value;
+  const guess = parseInt(guessValue); // Convert to integer for comparison
   // Access user input directly
 
   // CHECK IF EMPTY VALUE
   if (!guessValue) {
-    message.textContent = "⛔️ No number!";
+    displayMessage("⛔️ No number!");
     return;
   }
-  const guess = parseInt(guessValue); // Convert to integer for comparison
   if (isNaN(guess)) {
-    message.textContent = "⛔️ Please enter a valid number!";
+    displayMessage("⛔️ Please enter a valid number!");
     return;
   }
 
   if (guess >= 1) {
-    // when plaYER WINS
-    if (guess === randomNumber) {
-      wins();
-      // When guess is too High
-    } else if (guess > randomNumber) {
-      if (score > 1) {
-        message.textContent = "📈 Too high!";
-        score--;
-        document.querySelector(".score").textContent = score;
-      } else {
-        score = 0;
-        message.textContent = "You  Lost the Game";
-        document.querySelector(".score").textContent = score;
-      }
-      // When guess is too Low
-    } else if (guess < randomNumber) {
-      if (score > 1) {
-        message.textContent = "📉 Too low!";
-        score--;
-        document.querySelector(".score").textContent = score;
-      } else {
-        score = 0;
-        message.textContent = "You  Lost the Game";
-        document.querySelector(".score").textContent = score;
-      }
-    } else {
-      message.textContent = "🐷Oii🐷nnn🐷n🐷Kkkkk🐷!🐷";
-    }
+    checkWin(guess > randomNumber ? "toohigh" : "toolow");
   }
+  // When guess is too High
+  // } else if (guess > randomNumber) {
+  //   if (score > 1) {
+  //     displayMessage("📈 Too high!";
+  //     score--;
+  //     document.querySelector(".score").textContent = score;
+  //   } else {
+  //     score = 0;
+  //     displayMessage("You Lost the Game";
+  //     document.querySelector(".score").textContent = score;
+  //   }
+
+  //   // When guess is too Low
+  // } else if (guess < randomNumber) {
+  //   if (score > 1) {
+  //     displayMessage("📉 Too low!";
+  //     score--;
+  //     document.querySelector(".score").textContent = score;
+  //   } else {
+  //     score = 0;
+  //     displayMessage("You Lost the Game";
+  //     document.querySelector(".score").textContent = score;
+  //   }
+  else {
+    displayMessage("🐷Oii🐷nnn🐷n🐷Kkkkk🐷!🐷");
+  }
+  // }
 });
 
 btnAgain.addEventListener("click", function () {
@@ -67,7 +71,7 @@ btnAgain.addEventListener("click", function () {
   guessInput.value = ""; // Clear guess input
   randomNumber = Math.floor(Math.random() * 20) + 1;
 
-  message.textContent = "Start guessing...";
+  displayMessage("Start guessing...");
   score = 20;
   document.querySelector(".score").textContent = score;
   document.querySelector("body").style.backgroundColor = "#333";
@@ -77,7 +81,7 @@ btnAgain.addEventListener("click", function () {
   btnAgain.style.display = "none";
 });
 function wins() {
-  message.textContent = "🎉 Correct Number!";
+  displayMessage("🎉 Correct Number!");
   document.querySelector("body").style.backgroundColor = "#60b347";
   number.textContent = randomNumber;
   number.style.width = "30rem";
@@ -95,9 +99,28 @@ guessInput.addEventListener("input", () => {
   // Range validation and updating message based on validationMessage (optional)
   if (guessInput.value < 1 || guessInput.value > 20) {
     guessInput.setCustomValidity("Please enter a number between 1 and 20 ");
-    message.textContent = guessInput.validationMessage; // Update message based on validationMessage
+    displayMessage(guessInput.validationMessage); // Update message based on validationMessage
   } else {
     guessInput.setCustomValidity("");
-    message.textContent = ""; // Clear message element
+    displayMessage(""); // Clear message element
   }
 });
+
+function checkWin(output) {
+  if (output === "toohigh") {
+    score--;
+    displayMessage("📈 Too high!");
+  } else if (output === "toolow") {
+    score--;
+    displayMessage("📉 Too low!");
+  }
+  if (score === 0) {
+    displayMessage("You Lost the Game");
+  } else {
+    document.querySelector(".score").textContent = score;
+  }
+
+  if (Number(guessInput.value) === randomNumber) {
+    wins();
+  }
+}
